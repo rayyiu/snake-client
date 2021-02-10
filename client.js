18ab5a297ee66ad053ebe47aa02e9772b376f84e
Ray <rayyiu66@gmail.com>
2021-02-10 13:55:07 -0800

    and now ive saved them

--- client.js
+++ client.js
@@ -1,17 +1,4 @@
-const conn = net.createConnection({ 
-    host: 'localhost', // change to IP address of computer or ngrok host if tunneling
-    port: 3000 // or change to the ngrok port if tunneling
-  });
-  
-  conn.setEncoding('utf8'); // interpret data as text
-
-  const net = require('net');
-
-const server = net.createServer();
-
-server.listen(3000, () => {
-  console.log('Server listening on port 3000!');
-});
+const net = require('net');
 
 const connect = function() {
   const conn = net.createConnection({ 
@@ -26,3 +13,5 @@ const connect = function() {
 
 console.log('Connecting ...');
 connect();
+
+module.exports = {connect};
