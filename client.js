87cb067e920298c042b5e5ca1e2fac7084ef3470
Ray <rayyiu66@gmail.com>
2021-02-10 14:34:05 -0800

    added name

--- client.js
+++ client.js
@@ -7,8 +7,14 @@ const connect = function() {
   });
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
+  conn.on("connect", () => {
+    conn.write('Name: RY')
+  })
   conn.on('data', () => {
     console.log("you ded cuz you idled");
+  })
+  conn.on('connect', () => {
+    console.log("touchdown in the game server");
   })
 
   return conn;
