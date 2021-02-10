f5558160e0e13e15d18c180f12ee914372725c70
Ray <rayyiu66@gmail.com>
2021-02-10 15:04:19 -0800

    the snake moves

--- client.js
+++ client.js
@@ -7,14 +7,15 @@ const connect = function() {
   });
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
-  conn.on("connect", () => {
-    conn.write('Name: RY')
-  })
-  conn.on('data', () => {
-    console.log("you ded cuz you idled");
-  })
   conn.on('connect', () => {
     console.log("touchdown in the game server");
+  })
+  conn.on("connect", () => {
+    conn.write('Name: RY');
+    conn.write("Move: up")
+  
+  conn.on('data', (data) => {
+    console.log(data);
   })
 
   return conn;
